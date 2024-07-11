32281065b7038d18026341c1c152557f25af5cd0
Dr. Marius Zeinhofer <zeinhofe@ad.uniklinik-freiburg.de>
2024-07-11 10:33:14 +0200

    [ADD] toy_pca example on a dataset of 39 FDG PETs

--- examples/toy_pca.py
+++ examples/toy_pca.py
@@ -1,12 +1,10 @@
 """Extracts some principal components from a toy dataset."""
 
-import nibabel
 from pathlib import Path
-import jax.numpy as jnp
 
-import numpy as np
+import jax.numpy as jnp
+import nibabel
 from medclassx.pca import pca
-from matplotlib import pyplot as plt
 
 # collect data paths in a list
 paths = [p for p in Path(r"data\MCI39\nifti_preproc").iterdir()]
@@ -20,15 +18,13 @@ img_data = [n.get_fdata() for n in nifti_imgs]
 # convert to a single 4-d jax tensor of shape (batch, h, w, d)
 img_data = jnp.array(img_data)
 
-print(
-    f"Data group shape: {jnp.shape(img_data)} and dtype {img_data.dtype}."
-)
+print(f"Data group shape: {jnp.shape(img_data)} and dtype {img_data.dtype}.")
 
 # data shapes
 n, a, b, c = jnp.shape(img_data)
 
 # Reshape into matrix form
-X = jnp.reshape(img_data, shape=(n, a*b*c))
+X = jnp.reshape(img_data, shape=(n, a * b * c))
 
 # column center
 X -= jnp.mean(X, axis=0, keepdims=True)
@@ -42,14 +38,11 @@ X = X - jnp.min(X) + 1
 # log transform
 X = jnp.log(X)
 
-print(jnp.min(X), jnp.max(X))
-
 # perform pca
 transform, recover, singular_values, W = pca(X)
 
 
-
-pcs =  [jnp.reshape(w, shape=(a, b, c)) for w in jnp.transpose(W)]
+pcs = [jnp.reshape(w, shape=(a, b, c)) for w in jnp.transpose(W)]
 
 
 for i, pc in enumerate(pcs):
@@ -60,7 +53,4 @@ for i, pc in enumerate(pcs):
         header=nifti_imgs[0].header,
     )
 
-    nibabel.save(pc_to_nifti, "out\pc" + f"{i}" + ".nii")
-
-
-
+    nibabel.save(pc_to_nifti, "out\pc" + f"{i+1}" + ".nii")
